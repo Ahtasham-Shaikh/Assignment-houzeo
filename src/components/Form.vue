8d40da7600788bb00c0002f34710b0056ce3be91
Ahtasham-Shaikh <shaikh.ahtasham05263@gmail.com>
2024-07-12 02:51:05 +0530

    validations and error messages added

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,29 +5,29 @@
         <div class="field-wrapper flex">
           <label :for="'full-name-' + index">{{ obj.fields[0].fieldName }}: </label>
           <div class="flex fields">
-            <input v-model="obj.fields[0].value" type="text" :id="'full-name-' + index" :placeholder="obj.fields[0].fieldName" />
+            <input @input="onInvalidName" @invalid="onInvalidName" required minlength="6" class="name-field" v-model="obj.fields[0].value" type="text" :id="'full-name-' + index" :placeholder="obj.fields[0].fieldName" />
           </div>
         </div>
         <div class="field-wrapper flex">
           <label :for="'email-' + index">{{ obj.fields[1].fieldName }}: </label>
-          <input v-model="obj.fields[1].value" type="email" :id="'email-' + index" :placeholder="obj.fields[1].fieldName" />
+          <input required @invalid="onInvalidEmail" @input="onInvalidEmail" v-model="obj.fields[1].value" type="email" :id="'email-' + index" :placeholder="obj.fields[1].fieldName" />
         </div>
         <div class="field-wrapper flex">
           <label :for="'mobile-number-' + index">{{ obj.fields[2].fieldName }}</label>
-          <input maxlength="14" @input="handleMobInput(obj.fields[2])" v-model="obj.fields[2].value" type="text" :id="'mobile-number-' + index" :placeholder="obj.fields[2].fieldName" />
+          <input required @invalid="onInvalidMob" maxlength="14" @input="handleMobInput" v-model="obj.fields[2].value" type="text" :id="'mobile-number-' + index" :placeholder="obj.fields[2].fieldName" />
         </div>
         <button @click="removeGroupField(obj)" type="button" class="fit-content">Remove Fields</button>
       </div>
 
         <div>
         
-        <p class="error-p" v-if="!isTwoGroup">Minimum two group fields are required to submit the form</p>
+        <p class="error-p" v-show="!isTwoGroup">Minimum two group fields are required to submit the form</p>
       <button @click="addGroupField" type="button" class="fit-content">Add More Fields</button>
         </div>
 
       <div class="field-wrapper flex">
         <label :for="'dob-' + id.value">{{ dob.fieldName }}</label>
-        <input @change="formatDate" :max="today" v-model="dob.value" type="date" :id="'dob-' + id.value" />
+        <input required @change="formatDate" :max="today" type="date" :id="'dob-' + id.value" />
       </div>
       <div class="field-wrapper flex">
         <label :for="'gender-' + id.value">{{ gender.fieldName }}</label>
@@ -39,17 +39,19 @@
           <input v-model="gender.value" type="radio" :id="'others-' + id.value" name="gender" value="Others" />
           <label :for="'others-' + id.value">Others</label>
         </div>
+          <p v-show="!isGender && isSubmitted" class="error-p" id="gender-error">Please select your gender</p>
       </div>
       <div class="field-wrapper flex">
         <label :for="'react-' + id.value">{{ languages.fieldName }}</label>
         <div>
-          <input v-model="languages.value.React" type="checkbox" :id="'react-' + id.value" name="languages" value="React" />
+          <input class="cb" v-model="languages.value.React" type="checkbox" :id="'react-' + id.value" name="languages" value="React" />
           <label :for="'react-' + id.value">React</label>
-          <input v-model="languages.value.Vue" type="checkbox" :id="'vue-' + id.value" name="languages" value="Vue" />
+          <input class="cb" v-model="languages.value.Vue" type="checkbox" :id="'vue-' + id.value" name="languages" value="Vue" />
           <label :for="'vue-' + id.value">Vue</label>
-          <input v-model="languages.value.Angular" type="checkbox" :id="'angular-' + id.value" name="languages" value="Angular" />
+          <input class="cb" v-model="languages.value.Angular" type="checkbox" :id="'angular-' + id.value" name="languages" value="Angular" />
           <label :for="'angular-' + id.value">Angular</label>
         </div>
+          <p v-show="!isCBChecked && isSubmitted" id="language-error" class="error-p">Please select atleast on language</p>
       </div>
       <div class="field-wrapper flex">
         <label :for="'cities-' + id.value">{{ cities.fieldName }}</label>
@@ -64,10 +66,11 @@
           <option value="chennai">Chennai</option>
           <option value="los angeles">Los Angeles</option>
         </select>
+        <p v-show="!isCities && isSubmitted" class="error-p" id="cities-error">Please select atleast one city</p>
       </div>
       <div class="field-wrapper flex">
         <label >{{ file.fieldName }}</label>
-        <input @change="handleFileChange" type="file" accept=".jpeg, .pdf" />
+        <input required @change="handleFileChange" type="file" accept=".jpeg, .pdf" />
       </div>
       <input type="submit" value="Submit">
     </form>
@@ -77,9 +80,25 @@
 <script setup>
   import { reactive, ref, computed } from 'vue';
 
+    let isSubmitted = ref(false)
+
     let handleFileChange = (event) => {
-        let fileName = event.target.files[0].name
-        file.value = fileName
+      const input = event.target.files[0];
+      const allowedTypes = ['image/jpeg', 'image/png', 'application/pdf'];
+      
+      if (!input) {
+        event.target.setCustomValidity("Please select a file.");
+      } else if (!allowedTypes.includes(input.type)) {
+        event.target.setCustomValidity("Invalid file type. Please select a JPEG, PNG, or PDF file.");
+      } else {
+        event.target.setCustomValidity('');
+        file.value = input.name
+      }
+
+      // Triggering the invalid event if the file is not valid
+      if (event.target.validationMessage) {
+        event.target.reportValidity();
+      }
     }
 
     
@@ -105,7 +124,10 @@
     }
   ]);
 
-  const isTwoGroup = computed(() => groupFields.length >= 2);
+    let isTwoGroup = computed(() => groupFields.length >= 2);
+    let isCBChecked = computed(() => languages.value.React || languages.value.Vue || languages.value.Angular);
+    let isCities = computed(() => cities.value.length !== 0)
+    let isGender = computed(() => gender.value !== "")
 
   const removeGroupField = (obj) => {
     const index = groupFields.findIndex(o => o === obj);
@@ -124,8 +146,8 @@
 
   const emit = defineEmits('form-submitted');
 
-  const handleMobInput = (field) => {
-    let currVal = field.value.replace(/\D/g, '');
+  const handleMobInput = (event) => {
+    let currVal = event.target.value.replace(/\D/g, '');
     let formattedVal = '';
     if (currVal.length > 0) {
       formattedVal = '(' + currVal.substring(0, 3);
@@ -139,7 +161,9 @@
     if (currVal.length > 10) {
       formattedVal += ' ' + currVal.substring(10);
     }
-    field.value = formattedVal;
+    event.target.value = formattedVal;
+
+    onInvalidMob(event)
   };
 
   const formatDate = (event) => {
@@ -152,11 +176,53 @@
     dob.value = new Intl.DateTimeFormat('en-US', options).format(date);
   };
 
-  const handleSubmit = () => {
-    if(isTwoGroup.value){
+  const onInvalidName = (event) => {
+    const name = event.target
+    if(name.value.length === 0){
+      name.setCustomValidity('Name cannot be empty')
+    }
+    else if(name.value.length < 6){
+      name.setCustomValidity('Please enter your full name')
+    }
+    else{
+      name.setCustomValidity('')
+    }
+  }
+
+  const onInvalidEmail = (event) => {
+    let email = event.target
+    if(email.value.length === 0){
+      email.setCustomValidity("Enter your email address")
+      return false
+    }
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if(re.test(email.value)){
+      email.setCustomValidity("")
+    }
+    else{
+      email.setCustomValidity("Please enter a valid email")
+    }
+  }
+
+  const onInvalidMob = (event) => {
+    let mob = event.target;
+    if(mob.value.length === 0){
+      mob.setCustomValidity('Enter your mob phone')
+    }
+    else if(mob.value.length < 14){
+      mob.setCustomValidity('Enter a 10 digit number')
+    }
+    else{
+      mob.setCustomValidity('')
+    }
+  }
+
+const handleSubmit = (event) => {
+    isSubmitted.value = true
+    if(isTwoGroup.value && isCBChecked.value && isCities && isGender){
         emit('form-submitted', fieldsList, groupFields);
     }
-  };
+};
 
   const addGroupField = () => {
     groupFields.push({
